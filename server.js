ddbc2d414c5438fe9e25110878604a362ea56285
mantis-imanti <mantis@imanti.edu.mx>
2026-05-21 19:33:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -184,7 +184,8 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
-    console.log("Servidor corriendo en http://localhost:3000");
+server.listen(PORT, () => {
+    console.log("Servidor corriendo en puerto " + PORT);
 });
